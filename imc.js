4700a4663967dbfce98a92fbdf124dafdcbddcff
Geração Tech <geracaotech@digitalcollege.internal>
2025-04-24 15:07:24 -0300

    imc ajustes

--- imc.js
+++ imc.js
@@ -1,25 +1,22 @@
 const readline = require("readline-sync");
 
-let peso = parseFloat(readline.question("Qual o seu peso?"));
+let peso = parseInt(readline.question("Qual o seu peso?"));
 let altura = parseFloat(readline.question("Qual a sua altura?"));
 let imc=peso/(altura*altura);
+console.log("Seu IMC é " + (imc.toFixed(1)));
 
 if (imc < 18.5) {
-    console.log("Seu IMC é:" + imc);
     console.log("Abaixo do peso");
 }
 
-else if (imc >= 18.5 && imc <= 29.9){
-    console.log("Seu IMC é:" + imc);
+else if (imc >= 18.5 && imc <= 24.9){
     console.log("Peso normal");
 }
 
 else if (imc >=25 && imc <= 29.9){
-    console.log("Seu IMC é:" + imc);
-    console.log("Peso normal");
+    console.log("Sobrepeso");
 }
 
 else{
-    console.log("Seu IMC é:" + imc);
     console.log("Obesidade");
 }
